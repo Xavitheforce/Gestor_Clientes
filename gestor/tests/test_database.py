22e60abb23a6c140c9525097105914e3054cba59
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-01 18:19:43 +0200

    funcionan los test (pytest -v)

--- gestor/tests/test_database.py
+++ gestor/tests/test_database.py
@@ -1,7 +1,13 @@
 import csv
 import copy
-from gestor import config
-from gestor import helpers
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+import config
+import helpers
 import unittest
 import database as db
 
